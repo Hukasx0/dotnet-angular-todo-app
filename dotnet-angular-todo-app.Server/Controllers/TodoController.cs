86e2a08af7532c91b80ed47149f501d4f4cbaacc
Hubert Kasperek <hubertkasp13@gmail.com>
2024-10-29 17:03:22 +0100

    Replace WeatherForecastController with TodoController

---
+++ dotnet-angular-todo-app.Server/Controllers/TodoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_angular_todo_app.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        
+    }
+}
---
+++ dotnet-angular-todo-app.Server/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_angular_todo_app.Server.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+    }
+}
